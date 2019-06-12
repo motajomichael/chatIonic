e83a0820f027019716ef3a33feb60a37e2e01e6d
TurnQuest <michael.motajo@turnkeyafrica.com>
2019-06-12 21:41:08 +0100

    keys removed again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig : {
-        apiKey: "AIzaSyAhQxo5K6eHfs6TZmLwfGQub3Ye9fmjtbo",
-        authDomain: "chat21cloud.firebaseapp.com",
-        databaseURL: "https://chat21cloud.firebaseio.com",
-        projectId: "chat21cloud",
-        storageBucket: "chat21cloud.appspot.com",
-        messagingSenderId: "714297163771",
+        apiKey: "API-KEY",
+      authDomain: "AUTHDOMAIN",
+      databaseURL: "DBURL",
+      projectId: "ID",
+      storageBucket: "STORAGEBUCKET",
+      messagingSenderId: "MID",
     }
   }
